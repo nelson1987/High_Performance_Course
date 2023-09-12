e86fbbc019ef92bf5ef0250c6e7f39689a965a9c
Nelson Neto <nelson.ash@outlook.com>
2023-09-12 09:28:19 -0300

    Commit inicial

---
+++ src/FreeBank.Domain/Entities.cs
@@ -0,0 +1,28 @@
+﻿namespace FreeBank.Domain;
+
+public class Cliente
+{
+    public required List<Conta> Conta { get; set; }
+    public required string Email { get; set; }
+}
+public class Conta
+{
+    public required List<Movimentacao> Movimentacao { get; set; }
+    public required string Numero { get; set; }
+}
+public class ContaCorrente : Conta
+{
+
+}
+public class ContaInvestimento : Conta
+{
+
+}
+public class Movimentacao
+{
+    public required Conta Pagador { get; set; }
+    public required Conta Recebedor { get; set; }
+    public required decimal Valor { get; set; }
+    public required DateTime Data { get; set; }
+
+}
